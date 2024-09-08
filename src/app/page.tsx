c5b9252f8a4aca09593ab156a0294a8ad9beb309
Monika Luu <monika-luu@hotmail.com>
2024-09-08 18:16:01 +0200

    Add header and center content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,17 @@ import { FormEvent, useState } from "react";
 import Button from "./ui/button";
 import Input from "./ui/Input";
 import Label from "./ui/Label";
-import Link from "next/link";
 import Header from "./ui/header";
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState<boolean>(false);
+
   async function onSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
     setIsLoading(true);
 
     try {
       const formData = new FormData(event.currentTarget);
-      // const response = await fetch("TODO", {
-      //   method: "POST",
-      //   body: formData,
-      // });
-      // const data = await response.json();
       console.log(formData);
     } catch (error) {
       console.error(error);
@@ -30,25 +25,31 @@ export default function Home() {
 
   return (
     <>
-      <Header />
-      <main className="min-h-screen p-24 bg-primary flex flex-col justify-center items-center">
-        <h1 className="text-title1 text-headline">Eat Your Food</h1>
-        <form onSubmit={onSubmit}>
-          <div className="h-full w-full flex-col gap-y-5 flex">
-            <div>
-              <Label content="Username:" htmlFor="username" />
-              <Input type="text" name="username" />
-            </div>
-            <div>
-              <Label content="Password:" htmlFor="password" />
-              <Input type="password" name="password" />
-            </div>
-            <div className="flex flex-row justify-between">
-              <Button content="Log In" />
-              <Button content="Sign Up" />
-            </div>
+      <main className="min-h-screen bg-background">
+        <div className="w-full">
+          <Header />
+        </div>
+
+        <div className="flex flex-col justify-center items-center min-h-[calc(100vh-100px)]">
+          <div className="w-full max-w-md flex flex-col gap-y-5">
+            <form onSubmit={onSubmit}>
+              <div className="flex flex-col gap-y-5">
+                <div>
+                  <Label content="Username:" htmlFor="username" />
+                  <Input type="text" name="username" />
+                </div>
+                <div>
+                  <Label content="Password:" htmlFor="password" />
+                  <Input type="password" name="password" />
+                </div>
+                <div className="flex flex-row justify-between">
+                  <Button content="Log In" />
+                  <Button content="Sign Up" />
+                </div>
+              </div>
+            </form>
           </div>
-        </form>
+        </div>
       </main>
     </>
   );
--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -2,14 +2,22 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="flex flex-row justify-center w-full bg-background text-primary">
-      <Link className="m-5" href="/home">
-        Home
-      </Link>
-      <Link className="m-5" href="/products">
-        Products
-      </Link>
-    </div>
+    <>
+      <div className="flex flex-row justify-center w-full bg-headline text-primary ">
+        <Link
+          className="m-5 p-3  text-background hover:bg-tertiary hover:text-highlight"
+          href="/home"
+        >
+          Home
+        </Link>
+        <Link
+          className="m-5 p-3 text-background  hover:bg-tertiary hover:text-highlight "
+          href="/products"
+        >
+          Products
+        </Link>
+      </div>
+    </>
   );
 };
 
