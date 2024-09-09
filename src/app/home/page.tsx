58114b09c6280f78e2d7e42c66cb051e6411528c
Monika Luu <monika-luu@hotmail.com>
2024-09-09 12:09:01 +0200

    Fetch data from api

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,30 @@
+"use client";
+import axios from "axios";
+
 export default function Page() {
-  return <div>Home</div>;
+  const url = "https://kassal.app/api/v1/products";
+  const token = process.env.NEXT_PUBLIC_API_KEY;
+
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get(url, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log(response.data.data[0]);
+    } catch (error) {
+      console.error("Error: ", error);
+      throw error;
+    }
+  };
+
+  return (
+    <div>
+      Home
+      <button className="text-black" onClick={fetchProducts} type="button">
+        Fetch
+      </button>
+    </div>
+  );
 }
--- src/app/ui/button.tsx
+++ src/app/ui/button.tsx
@@ -1,14 +1,16 @@
 interface ButtonProps {
   content: string;
+  onClick?: () => void;
 }
 
-function Button({ content }: ButtonProps) {
+function Button(props: ButtonProps) {
   return (
     <button
+      {...props}
       type="submit"
       className="bg-button text-buttonText rounded py-2 px-4 hover:cursor-pointer hover:bg-primary border-stroke m-1"
     >
-      {content}
+      {props.content}
     </button>
   );
 }
